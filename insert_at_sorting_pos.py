427f72a0ad92aac6a951970687589ca3a141e214
Yashraj Tilekar <yashtilekar25@gmail.com>
2024-05-05 14:23:11 +0530

    Session-08

--- insert_at_sorting_pos.py
+++ insert_at_sorting_pos.py
@@ -1,13 +1,24 @@
-List = [10 ,20 ,30 ,40 ,50 ,15]
+import sys
 
-print("Before L : " ,List)
+def insertAtSortingPos(numList):
+    N = len(numList)
+    key = numList[N - 1] #last element
+    i = N - 2  
 
-key = List[len(List) - 1]
+    while(i >= 0 and numList[i] > key):
+        numList[i + 1] = numList[i]
+        i = i - 1
 
-i =len(List) - 2
-while(i > -1 and List[i] > key):
-    List[i+1] = List[i]
-    i = i - 1
+    numList[i + 1 ] = key
 
-List[i+1] = key
-print("After L : " ,List) 
+
+def main():
+    data = [10 ,20 ,30 ,40 ,50 ,60 ,15]
+    
+    print("Before" ,data)
+    insertAtSortingPos(data)
+    print("After" ,data)
+
+    sys.exit()
+
+main()
--- insertion_sort.py
+++ insertion_sort.py
@@ -39,14 +39,60 @@ def showList(numList : list[int] ,msg : str) -> None:
 
 def insertAtSortingPosition(numList : list[int] ,N : int) -> None:
     """
-    Args:
-        numList (list[int]): non-empty li
-        N (int): _description_
+     @input: 
+        @L: non-empty list of integers 
+        @N: length of list (not necessarily equal to len(L))
+    @output: 
+        None 
+    @precondition: 
+        1) N >= 2 
+        2) L[0] to L[N-2] are sorted in non-decreasing order 
+        3) Value of L[N-1] may be preventing from L[0] to L[N-1] 
+            from being sorted
+    @postcondition: 
+        1) L[0] to L[N-1] is sorted  
     """
-
     if(type(numList) != list or type(N) != int):
         raise TypeError("Bad type for input data")
     if(N < 2):
         raise ValueError("N must be greater than or equal to 2")
     
+    key = numList[N - 1]
+    i = N - 2
+
+    while(i >= 0 and numList[i] > key):
+        numList[i + 1] = numList[i]
+        i = i - 1 
     
+    numList[i + 1] = key
+
+def insertionSort(numList : list[int]) -> None :
+    """
+    @input: 
+        @L: L is a non-empty list of integers with no order 
+    @output: 
+        @L is sorted in place 
+    """ 
+
+    if(type(numList) != list):
+        raise TypeError("Bad input data")
+    elif(len(numList) <= 1):
+        return None
+    
+    N = len(numList)
+
+    for i in range(2 ,N+1):
+        insertAtSortingPosition(numList ,i)
+    
+def main(): 
+    N = int(input("Enter the size of the list:(greater than 2):"))
+    if N < 2: 
+        print("Bad size")
+        sys.exit(-1)
+
+    L = getList(N)
+    showList(L, "Before sort:")
+    insertionSort(L)
+    showList(L, "After sort:")
+
+main()    
